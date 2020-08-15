c370558671412a8a08fb1a8640165214933c7be3
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-08-15 11:55:49 -0400

    add zhihu_answer

--- index.js
+++ index.js
@@ -22,15 +22,20 @@ function datestr(y, m, d){
 hexo.extend.tag.register('source', function(args){
 	var source = args[0];
 	var id = args[1];
+	var cls, platform, link;
 	datestring = datestr(args[2], args[3], args[4]);
 	if (source == "zhihu"){
-		var cls = "info zhihu"; 
-		var platform = "知乎专栏";
-		var link = "https://zhuanlan.zhihu.com/p/" + id;
+		cls = "info zhihu"; 
+		platform = "知乎专栏";
+		link = "https://zhuanlan.zhihu.com/p/" + id;
 	}else if( source == "wechat"){
-		var cls = "success wechat"; 
-		var platform = "微信公众号";
-		var link = "https://mp.weixin.qq.com/s/" + id;
+		cls = "success wechat"; 
+		platform = "微信公众号";
+		link = "https://mp.weixin.qq.com/s/" + id;
+	}else if ( source == "zhihu_answer"){
+		cls = "info zhihu";
+		platform = "知乎";
+		link = "https://www.zhihu.com/answer/" + id;
 	}
 	return `<div class="note sourcenote ${cls}">本文${datestring}发表于${platform}，<a href="${link}">查看原文</a></div>`;
 });
