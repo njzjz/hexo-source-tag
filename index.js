855b2bca926f9641cbae69db9eded2e53f803f90
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-10 20:51:31 -0500

    fix: add i18n support

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const css = hexo.extend.helper.get('css').bind(hexo);
 const { name, version } = require('./package.json');
+const { format } = require('util');
+
+var i18n = new require('hexo-i18n')({
+    languages: ['en', 'zh-CN']
+});
+
+i18n.set('en', require('./locales/en.json');
+i18n.set('zh-CN', require('./locales/zh-CN.json');
 
 function cdn_url(path){
 	return `https://cdn.jsdelivr.net/npm/${name}@${version}/${path}`;
@@ -17,38 +25,26 @@ hexo.extend.injector.register('head_end', () => {
 	return css(cdn_url('css/source.css'));
 }, 'page');
 
-function datestr(y, m, d){
-	datesting = "";
-	if (y){
-		datesting += "于"+ y.toString() + "年";
-		if (m){
-			datesting += m.toString() +"月";
-			if (d){
-				datesting += d.toString() +"日";
-			}
-		}
-	}
-	return datesting;
+function datestr(__, y, m, d){
+	if (y&&m&&d) return __('dateymd', y, m, d);
+	else if (y&&m) return __('dateym', y, m);
+	else if (y) return __('datey', y);
+	else return '';
 }
 
-
+platform_info = require('./platform.json');
 hexo.extend.tag.register('source', function(args){
+	const lang = this.page.lang;
+	var __ = i18n.__(lang);
 	var source = args[0];
 	var id = args[1];
 	var cls, platform, link;
-	datestring = datestr(args[2], args[3], args[4]);
-	if (source == "zhihu"){
-		cls = "info zhihu"; 
-		platform = "知乎专栏";
-		link = "https://zhuanlan.zhihu.com/p/" + id;
-	}else if( source == "wechat"){
-		cls = "success wechat"; 
-		platform = "微信公众号";
-		link = "https://mp.weixin.qq.com/s/" + id;
-	}else if ( source == "zhihu_answer"){
-		cls = "info zhihu";
-		platform = "知乎";
-		link = "https://www.zhihu.com/answer/" + id;
-	}
-	return `<div class="note sourcenote ${cls}">本文${datestring}发表于${platform}，<a href="${link}">查看原文</a></div>`;
-});
+	datestring = datestr(__, args[2], args[3], args[4]);
+	cls = platform_info[source].cls;
+	link = platform_info[source].link;
+	platform = __('platform.'+source);
+	published_str = __('publish', datestring, platform);
+	raw_str = __('raw');
+	return `<div class="note sourcenote ${cls}">${published_str}<a href="${link}">${raw_str}</a></div>`;
+}, {async: true});
+
