53f8032cf1ebd4764fcc8f30692d032decfb5d8c
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-10 23:32:53 -0500

    bugfix

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function datestr(t, y, m, d){
 
 platform_info = require('./platform.json');
 hexo.extend.tag.register('source', function(args){
-	const lang = this.page.lang;
+	const lang = this.lang;
 	var t = i18n[lang];
 	var source = args[0];
 	var id = args[1];
