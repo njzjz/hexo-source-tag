c6b43b8e877ff1089c837b26094869c2ed9a8d1b
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-02-14 00:47:01 -0500

    fix: use packages

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ const { name, version } = require('./package.json');
 const format = require('string-format');
 const Injector = require("hexo-tag-injector");
 const injector = new Injector(hexo);
+const { npm_url } = require("jsdelivr_url");
+const { htmlTag } = require("hexo-util");
 
 const i18n = {
-    'en': require('./locales/en.json'),
-    'zh-CN': require('./locales/zh-CN.json')
+	'en': require('./locales/en.json'),
+	'zh-CN': require('./locales/zh-CN.json')
 }
 
-function cdn_url(path){
-	return `https://cdn.jsdelivr.net/npm/${name}@${version}/${path}`;
-}
-
-function datestr(t, y, m, d){
-	if (y&&m&&d) return format(t['dateymd'], y, m, d);
-	else if (y&&m) return format(t['dateym'], y, m);
+function datestr(t, y, m, d) {
+	if (y && m && d) return format(t['dateymd'], y, m, d);
+	else if (y && m) return format(t['dateym'], y, m);
 	else if (y) return format(t['datey'], y);
 	else return '';
 }
 
 platform_info = require('./platform.json');
-hexo.extend.tag.register('source', function(args){
+hexo.extend.tag.register('source', function (args) {
 	const lang = args[5] || this.lang || 'zh-CN';
 	var t = i18n[lang];
 	var source = args[0];
@@ -33,10 +31,15 @@ hexo.extend.tag.register('source', function(args){
 	platform = t['platform'][source];
 	published_str = format(t['publish'], datestring, platform);
 	raw_str = t['raw'];
-	return injector.mark(`<div class="note sourcenote ${cls}">${published_str}<a href="${link}">${raw_str}</a></div>`);
-}, {async: true});
+	return injector.mark(htmlTag(
+		'div',
+		{ class: `note sourcenote ${cls}` },
+		`${published_str}<a href="${link}">${raw_str}</a>`,
+		false,
+	));
+}, { async: true });
 
 injector.register('head_end', css({
-    href: cdn_url('css/source.min.css'),
-    class: 'pjax',
+	href: npm_url(name, version, 'css/source.min.css'),
+	class: 'pjax',
 }), remove_from_index = true);
